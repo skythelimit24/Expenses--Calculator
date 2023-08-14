849b139e4d8e762f4c4af02a9f2cd915243fdc39
skythelimit24 <akash.tanwar101@gmail.com>
2023-08-14 18:14:25 +0530

    Final Day 3

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,7 +1,14 @@
+import React,{useState} from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expense.css'
 import Card from './Card';
-function Expense() {
+import ExpenseFilter from './ExpenseFilter';
+function Expense(props) {
+    const [filteredYear, setFilteredYear] = useState('2020');
+  
+    const filterChangeHandler = selectedYear => {
+      setFilteredYear(selectedYear);
+    };
   const expenses = [
     {
       id: 'e1',
@@ -36,6 +43,7 @@ function Expense() {
   ];
   return (
     <Card className="expenses">
+      <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
       <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} />
       <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} />
       <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} />
